ed0f1bde0627e98cbb4491fc6fbba48ac02766d6
Amjad Jibon <amjad.jibon@gmail.com>
2021-06-28 11:27:44 +0600

    error update

--- pkg/spooky/mylocalip.go
+++ pkg/spooky/mylocalip.go
@@ -2,22 +2,25 @@ package spooky
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 	"net"
 )
 
-// GetOutboundIP Get preferred outbound ip of this machine
+// getOutboundIP Get preferred outbound ip of this machine
 func getOutboundIP() (net.IP, error) {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		return nil, err
 	}
-	defer func() error {
-		if err := conn.Close(); err != nil {
-			return err
+
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Error(err)
+			return
 		}
-		return nil
-	}()
+	}(conn)
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP, nil
